cde58b35edfded5bf355fcf8b72e1acc5eecd858
kavishdahekar <kavishdahekar@gmail.com>
2015-12-31 13:25:29 +0530

    File opening and initial message box.

--- endec.py
+++ endec.py
@@ -1,18 +1,20 @@
 #simple custom encryption decryption
 
 from pyDes import *
-import easygui as gui
+from easygui import *
 from sys import argv
 
-script, mode = argv
+# script, mode = argv
 
-file = gui.fileopenbox()
-file_contents = file.read()
-
-msg = "I'm only offering you the truth, nothing more?"
-	title = "Choose"
-	if ccbox(msg, title):     # show a Continue/Cancel dialog
-		pass  # user chose Continue
-	else:  # user chose Cancel
-		sys.exit(0)          
 	
+msg = "I'm only offering you the truth, nothing more?"
+title = "Choose Neo"
+if boolbox(msg, title, ["Red Pill", "Blue Pill"]):
+	mode = "encr"
+else:
+	mode = "decr"
+
+file_path = fileopenbox()
+file = open(file_path)
+file_contents = file.read()
+print file_contents
